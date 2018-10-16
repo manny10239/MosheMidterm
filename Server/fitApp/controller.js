5ab7e79ed80bffe012625292ad090a7b7d1aba00
Manny <mannyflory9@gmail.com>
2018-10-16 12:32:21 -0400

    added put fucntion to add friend to player array

--- Server/fitApp/controller.js
+++ Server/fitApp/controller.js
@@ -7,16 +7,17 @@ const app = express.Router();
 app.get("/", (req, res) => {
     res.send(fitapp);
 });
-
+//makes a players
 app.post('/players', (req , res) => {
     const player = {
         id: fitapp.users.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        friends: []
     };
     fitapp.users.push(player);
     res.send(player);
 });
-
+// gets the player with its id
 app.get('/players/:id', (req,res) => {
     const player = fitapp.users.find(c => c.id === parseInt(req.params.id));
     if(!player){
@@ -24,7 +25,7 @@ app.get('/players/:id', (req,res) => {
     }
     res.send(player);
 });
-
+//changes with id a player name
 app.put('/players/:id', (req,res) => {
     const player = fitapp.users.find(c => c.id === parseInt(req.params.id));
     if(!player){
@@ -32,6 +33,17 @@ app.put('/players/:id', (req,res) => {
     }
     player.name = req.body.name;
     res.send(player);
+});
+
+//add friend to friend array
+app.put('/players/addfriend/:id', (req, res) => {
+    const player = fitapp.users.find(c => c.id === parseInt(req.params.id));
+    if(!player){
+        res.status(404).send('The user with given ID was found');
+    }
+    const user = fitapp.users.find(c => c.id === 1);
+    user.friends.push(player);
+    res.send(user);
 });
 
 module.exports = app;
