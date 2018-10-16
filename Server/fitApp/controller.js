6ffecb3591d4a854e7554cb26a2f0bedfe2316a8
Manny <mannyflory9@gmail.com>
2018-10-16 01:20:56 -0400

    added functionality to add get change a user

--- Server/fitApp/controller.js
+++ Server/fitApp/controller.js
@@ -9,8 +9,28 @@ app.get("/", (req, res) => {
 });
 
 app.post('/players', (req , res) => {
-    const player = new Player(req.body.name);
+    const player = {
+        id: fitapp.users.length + 1,
+        name: req.body.name
+    };
     fitapp.users.push(player);
+    res.send(player);
+});
+
+app.get('/players/:id', (req,res) => {
+    const player = fitapp.users.find(c => c.id === parseInt(req.params.id));
+    if(!player){
+        res.status(404).send('The user with given ID was found');
+    }
+    res.send(player);
+});
+
+app.put('/players/:id', (req,res) => {
+    const player = fitapp.users.find(c => c.id === parseInt(req.params.id));
+    if(!player){
+        res.status(404).send('The user with given ID was found');
+    }
+    player.name = req.body.name;
     res.send(player);
 });
 
--- Server/fitApp/model.js
+++ Server/fitApp/model.js
@@ -1,20 +1,14 @@
 const friends = [
 
 ];
-class Player{
-    constructor(name){
-        this.name = name;
-        this.points = 0;
-    }
-}
 class FitnessApp{
     constructor(){
         this.users = [];
         this.workouts = [];
         this.workDone = [];
-        console.log("The app constructor");
+        
     }
 }
 module.exports = {
-    Player , FitnessApp
+    FitnessApp
 }
