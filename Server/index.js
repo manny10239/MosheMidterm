40c7b4fb1ec1f611f34e33482f3895cbdee9989d
Manny <mannyflory9@gmail.com>
2018-10-15 21:20:33 -0400

    setting up port to list on

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+const port = process.env.port || 3000;
+const server = "localhost";
+
+app.use(express.json());
+app.use("/", express.static(__dirname + "/../client/"));
+
+app.listen(port);
+console.log(`Listening on: http://${server}:${port}`);
+
